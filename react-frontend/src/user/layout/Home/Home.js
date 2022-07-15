98ee5e465bffc80f10365bc5cbd3767bfc9f74a1
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-07-15 13:44:46 +0700

    insert Home folder

---
+++ react-frontend/src/user/layout/Home/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getProducts } from "./redux/actions/productsActions";
+import { getCart } from "./redux/actions/cartAction";
+import { getCategories } from './redux/actions/categoriesActions';
+import { getCategoriesRoot } from "./redux/actions/categoryRootActions";
+
+import HomePage from "./HomePage";
+class Home extends Component {
+  componentDidMount() {
+    this.props.getProducts();
+    this.props.getCart();
+    this.props.getCategoriesRoot();
+    this.props.getCategories();
+  }
+
+  render() {
+    const { products, cart, categories, categoriesRoot } = this.props;
+    return (
+      <div>
+        <HomePage products={products} cart={cart} categories={categories} categoriesRoot={categoriesRoot} />
+      </div>
+    );
+  }
+}
+Home.propTypes = {
+  getProducts: PropTypes.func.isRequired,
+  getCart: PropTypes.func.isRequired,
+  getCategoriesRoot: PropTypes.func.isRequired,
+  getCategories: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  products: state.product.products,
+  cart: state.cart.cart,
+  categoriesRoot: state.categoriesRoot.categoriesRoot,
+  categories: state.categories.categories
+});
+
+export default connect(mapStateToProps, { getProducts, getCart, getCategoriesRoot, getCategories })(Home);
