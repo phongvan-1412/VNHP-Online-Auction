43a59c3949c93bbaeda42ec7460c4d2b784616fc
phuongvu <phuongvx@greenbeli.io>
2022-07-17 09:42:33 +0700

    Renew project

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,23 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import "./user/css/style-mobile.css";
-import "./user/css/style-tablet.css";
-import "./user/css/style-laptop.css"
+// import "./user/css/style-mobile.css";
+// import "./user/css/style-tablet.css";
+// import "./user/css/style-laptop.css"
 
-
-import store from "./redux/store";
 import { Provider } from "react-redux";
-import Home from "./Home";
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <Home />
-        </Router>
-      </Provider>
+     
+          <div>123</div>
+       
     );
   }
 }
