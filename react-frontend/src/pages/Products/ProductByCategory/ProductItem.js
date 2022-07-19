f28c70164775705059e626fce8b28262792b9e38
phuongvu <phuongvx@greenbeli.io>
2022-07-20 01:11:09 +0700

    Add countdown product item

--- react-frontend/src/pages/Products/ProductByCategory/ProductItem.js
+++ react-frontend/src/pages/Products/ProductByCategory/ProductItem.js
@@ -7,8 +7,41 @@ const ProductItem = ({ product }) => {
   const onProductClicked = (e) => {
     $("#data").data("productid", e.target.name);
   };
+
+  //countdown test 
+  var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("progressBar").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("progressBar").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
   return (
     <div className="product-grid">
+      <div type="text" id="progressBar" style={{ marginLeft: "100px" }}></div>
+
       <div>
         <Link
           to={`/${product.category_name}/${product.product_name}`}
