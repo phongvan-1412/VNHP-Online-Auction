dbc3d4f6a997836d0525bd9ddf8cbd9ce6bac76f
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-07-26 01:03:29 +0700

    edit link for LandingPageCategories

--- react-frontend-user/src/pages/LandingPageSlider/LandingPageCategories.js
+++ react-frontend-user/src/pages/LandingPageSlider/LandingPageCategories.js
@@ -12,11 +12,13 @@ const LandingPageCategories = () => {
             </div>  
         </div>        
     );
+
     function getData() {
         fetch("http://127.0.0.1:8000/api/selectcategories", { method: "GET" })
           .then((res) => res.json())
           .then((res) => {
             res.map((category) => {
+                
                 const tmp = document.createElement("a");
                     tmp.id = "category-grid" + category.category_id; 
 
@@ -24,16 +26,17 @@ const LandingPageCategories = () => {
                     tmp_img.src = require('../../img/LandingPage/Fine Art.jpg');
                     tmp.append(tmp_img);
 
-            
                 const tmp_imgname_link = document.createElement("a");
                     tmp_imgname_link.className = "landingpage-category-item-name";
+                    tmp_imgname_link.id = category.category_name.replace(/-/g, " ");
                     tmp_imgname_link.innerHTML = category.category_name.replace(/-/g, " ");
+                    tmp_imgname_link.onclick = (e) => {window.location.href = `http://localhost:3000/category/${category.category_name}`} ;
                     tmp.append(tmp_imgname_link);
                     
                  tmp.className = "landingpage-category-item-wrapper";
 
                 $("#landingpage-category-grid").append(tmp);
-
+                
             })
           }
     )}
