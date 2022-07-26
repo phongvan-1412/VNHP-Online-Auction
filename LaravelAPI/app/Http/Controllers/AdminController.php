710fe1aed2264656fd0c0b7c93bc311a696088cc
unknown <nhan.nguyenkieu317@gmail.com>
2022-07-26 13:49:58 +0700

    update login

--- LaravelAPI/app/Http/Controllers/AdminController.php
+++ LaravelAPI/app/Http/Controllers/AdminController.php
@@ -10,13 +10,9 @@ class AdminController extends Controller
     public function Login(Request $request){
 
         $admin = Admin::select()->where('emp_email',$request->email)
-                                ->where('emp_pwd',md5($request->pwd))
-                                ->first();
-        if($admin){
-            return 1;
-        }else{
-            return 0;
-        }
+                                ->where('emp_pwd',$request->pwd)
+                                ->get();
+        return $admin;
 
     }
 }
