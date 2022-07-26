52aaf04721e18512e167f24407ddbdae841cb14d
HANHTRAN2710 <tranngochanh27102001@gmail.com>
2022-07-26 19:25:12 +0700

    update addcategory

--- LaravelAPI/app/Http/Controllers/CategoryAPI.php
+++ LaravelAPI/app/Http/Controllers/CategoryAPI.php
@@ -17,24 +17,19 @@ class CategoryAPI extends Controller
     public function AddCategory(Request $request){
         $newCategory = new Category();
         $newCategory->category_name =  $request->category_name;
-
         $category_img = $request->file('user_img_name');
         $newCategory->category_img_name = time().'-'.'category.'. $request->img_extension;
-        $category_img->move(public_path('CategoryImg'),  time().'-'.'category.'.$request->img_extension);
 
-       
-       
-       
-        $isExist = Category::select()->where('category_name',  $newCategory->category_name)->exists();
+        $isExist = Category::select()->where('category_name',  $request->category_name)->exists();
 
         if(!$isExist)
         {
             $newCategory->save();
+            $category_img->move(public_path('CategoryImg'),  time().'-'.'category.'.$request->img_extension);
             return 1;
         }
        
         return 0;         
-            
     }
     // table add category
     public function AddCategoryTable(){
