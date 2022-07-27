84dae5cdc3d5b4eb8ea076c6482a73c27b6a9e87
phuongvu <phuongvx@greenbeli.io>
2022-07-27 12:02:30 +0700

    Update product model

--- LaravelAPI/app/Models/Product.php
+++ LaravelAPI/app/Models/Product.php
@@ -8,20 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-
-    public $product_id;
-    public $product_SKU;
-    public $product_name;
-    public $category_id;
-    public $product_quatity;
-    public $product_information;
-    public $product_ingredients;
-    public $product_instruction_use;
-    public $product_instruction_store;
-    public $product_img_name;
-    public $product_price_per_unit;
-    public $product_status;
-    public $category_name;
-    public $category_root;
-    public $category_status;
+    protected $table = 'product';
+    protected $primaryKey = 'product_id';
+    public $timestamps = false;
+    protected $fillable = [
+        'product_id',
+        'owner_id',
+        'product_SKU',
+        'product_name',
+        'category_id',
+        'product_information',
+        'product_ingredients',
+        'product_instruction_use',
+        'product_instruction_store',
+        'product_thumbnail_img_name',
+        'product_img_name1',
+        'product_img_name2',
+        'product_img_name3',
+        'product_start_price',
+        'product_price_aution',
+        'product_start_aution_day',
+        'product_end_aution_day',
+        'product_status',
+    ];
 }
