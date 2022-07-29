7d840fc2350d962d7efea787d2552872a4bdf546
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-07-29 22:41:59 +0700

    edit pagination

--- react-frontend-user/src/pages/Products/ProductByCategory/Products.js
+++ react-frontend-user/src/pages/Products/ProductByCategory/Products.js
@@ -4,45 +4,13 @@ import $ from "jquery";
 import ProductView from "./ProductView";
 
 const Products = ({products,category}) => {
+  let currentProducts = [];
 
-  // get current category
-  // let cate = [];
-  // let check = false;
-  // if (category_id <= 5) {
-  //   check = true;
-  //   categoriesroot.forEach((category) => {
-  //     if (category.category_id == category_id) {
-  //       cate = category;
-  //     }
-  //   });
-  // } else {
-  //   categories.forEach((category) => {
-  //     if (category.category_id == category_id) {
-  //       cate = category;
-  //     }
-  //   });
-  // }
-
-  // get products
-  // let currentProducts = [];
-
-  // if (category_id <= 5) {
-  //   categories.forEach((cate) => {
-  //     products.forEach((product) => {
-  //       if (cate.category_root == category_id) {
-  //         if (product.category_id == cate.category_id) {
-  //           currentProducts = [...currentProducts, product];
-  //         }
-  //       }
-  //     });
-  //   });
-  // } else {
-  //   products.forEach((product) => {
-  //     if (product.category_id == category_id) {
-  //       currentProducts = [...currentProducts, product];
-  //     }
-  //   });
-  // }
+  products.forEach((product) => {
+    if (product.category_id == category.category_id) {
+      currentProducts = [...currentProducts, product];
+    }
+  });
 
   function onClick(e) {
     $("#data").data("productbycate", e.target.name);
@@ -51,16 +19,15 @@ const Products = ({products,category}) => {
   const productsPerPage = 8;
   const currentPage = 1;
 
-  //Change page
   const paginate = (pageNumber) => currentPage = pageNumber;
   let pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(products.length / productsPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(currentProducts.length / productsPerPage); i++) {
     pageNumbers.push(i);
   }
 
   return (
-    <div className="row">
+    <div className="container">
       <div className="row" style={{ padding: "0px", margin: "0px" }}>
         <div className="container" style={{ padding: "0px", margin: "0px" }}>
           <div className="row">
@@ -68,17 +35,11 @@ const Products = ({products,category}) => {
             <div className="wrap-breadcrumb" style={{ margin: "50px 0px" }}>
               <ul>
                 <li className="item-link">
-                  <Link replace to="/" className="home-link">
-                    HOME
-                  </Link>
+                  <Link replace to="/" className="home-link">HOME</Link>
                 </li>
+
                 <li className="item-link1">
-                  <Link
-                    to={`/${category.category_name}`}
-                    name={category.category_id}
-                    replace
-                    onClick={onClick}
-                  >
+                  <Link to={`/${category.category_name}`} name={category.category_id} replace onClick={onClick}>
                     {category.category_name.replace(/-/g, " ")}
                   </Link>
                 </li>
