cd37539bc70c84805dade1cb423daf2afbb40866
phuongvu <phuongvx@greenbeli.io>
2022-07-31 10:05:08 +0700

    ADd route check exists category API

--- LaravelAPI/routes/api.php
+++ LaravelAPI/routes/api.php
@@ -29,6 +29,7 @@ Route::post('/addcategory',[CategoryAPI::class,'AddCategory']);
 Route::get('/addcategorytable',[CategoryAPI::class,'AddCategoryTable']);
 Route::get('/addcategorytable',[CategoryAPI::class,'AddCategoryTable']);
 
+Route::get('/checkexistscategory',[CategoryAPI::class,'CheckExistsCategory']);
 
 //PRODUCTS
 Route::get('/selectallproducts',[ProductAPI::class, 'SelectProducts']);
