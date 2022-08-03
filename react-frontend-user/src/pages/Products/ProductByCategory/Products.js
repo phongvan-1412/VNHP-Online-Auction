c830b8bb3de36429893fee0c33b77b8aec761bf0
phuongvu <phuongvx@greenbeli.io>
2022-08-04 01:27:39 +0700

    Delete trash
    Signed-off-by: phuongvu <phuongvx@greenbeli.io>

--- react-frontend-user/src/pages/Products/ProductByCategory/Products.js
+++ react-frontend-user/src/pages/Products/ProductByCategory/Products.js
@@ -5,11 +5,6 @@ import $ from 'jquery';
 //Slider
 import { Slider } from "@material-ui/core"
 
-//Filter Date
-import { DateRange } from 'react-date-range';
-import 'react-date-range/dist/styles.css'; 
-import 'react-date-range/dist/theme/default.css'; 
-
 import ProductView from "./ProductView";
 
 const Products = ({ products, category, categories }) => {
