0dd5420a5bc5dcda6d7a8c8143b8dd2355999b07
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-08-06 17:07:02 +0700

    css responsive

--- react-frontend-user/src/pages/TermAndPolicy/TermAndPolicy.js
+++ react-frontend-user/src/pages/TermAndPolicy/TermAndPolicy.js
@@ -6,75 +6,108 @@ class TermAndCondition extends Component {
       <div className="container">
         <div>
           <div>
-            <h3>GENERAL CONDITIONS:</h3>
+            <h1>GENERAL CONDITIONS:</h1>
+            <h5>INVALUABLE, INVALUABLE LIVE AUCTIONS AND INVALUABLE TIMED AUCTIONS TERMS OF USE AGREEMENT</h5>
+            <span>This document describes terms and conditions applicable to your use of the services made available by Invaluable, LLC ("Invaluable", "we", or "us") at the websites Invaluable.com, VNHPOnlineAuction.com, and Infinite Bidding Sites (the "Sites").</span>
           </div>
+
+        <h3>Section 1. General.</h3>
           <div>
             <ul>
               <li>
-                The E-commerce website (https://shop.annam-gourmet.com/) is
-                managed by Gourmet Distribution Co., Ltd (“Annam Gourmet”).
+                <h5>1.1 Services.</h5>
+                <span>
+                This Terms and Conditions of Use Agreement (hereafter this "Agreement") sets forth the general terms and conditions that apply to the use by you of the Sites, including the features and services offered by us from time to time at or through the Sites (collectively with the Sites, the "Services"). BY USING THE SERVICES, YOU AGREE TO THE TERMS AND CONDITIONS CONTAINED HEREIN. This Agreement is a legally binding instrument between you and Invaluable, and describes your responsibilities in connection with your use of the Services and, among other things, limits the liability of Invaluable. Before using any of the Services, please read all of this Agreement carefully. By accessing or using any Services, you affirm that you are over 18 years of age and are otherwise capable of forming legally binding contracts, and that you agree to be legally bound and to abide by this Agreement. If you are under 18 years of age, or are otherwise incapable of forming legally binding contracts, or do not agree with any part of this Agreement, YOU MUST NOT ACCESS OR USE THE SERVICES.
+                </span>
+
               </li>
               <li>
-                All products on our website fully comply with the Government
-                regulations. We guarantee not to sell counterfeit or illegal
-                products without custom claimed, and unknown origin goods. Our
-                products are qualified in line with strict technical standards,
-                quality control, and food safety, product origins and labels.
-                All activities on the website are conducted publicly,
-                transparently, and legitimately.
+                <h5>1.2 Amendments.</h5>
+                <span>
+                We reserve the right, exercisable in our sole discretion, to change, modify, add to, subtract from, or otherwise amend the terms and conditions of this Agreement at any time. Except as otherwise stated below, all changes, modifications, or other amendments shall be effective on a prospective basis once they are posted on our Sites. This Agreement may not otherwise be amended except in a writing signed by you and Invaluable. Continued use of the Services by you constitutes your binding acceptance of this Agreement, including any changes or modifications made by us as described above. You agree to review the terms and conditions of this Agreement periodically to become aware of such revisions and to review your compliance with them. If at any time the terms and conditions of this Agreement are no longer acceptable to you, you must immediately cease all use of the Services. The right to access and use the Services is personal to you and is not transferable to any other person or entity.
+                </span>
+ 
               </li>
               <li>
-                By accessing and using this website, you acknowledge that you
-                understand and agree with these Terms and Conditions. Annam
-                Gourmet reserves the right to amend, change, edit, add or remove
-                any part of these Terms and Conditions at any time ("Changes").
-                Any such Changes will be effective immediately upon public
-                posting without prior notice. And when you continue to use the
-                website, it means you accept such Changes after public posting.
+                <h5>1.3 Additional Agreements.</h5>
+                <span>
+                The Services include, among other things, such services as: contacting or placing bids with auction houses by email or through a live bidding console; searching and accessing past and upcoming auction records including, when available, object photos and estimated and realized prices; looking up or searching for artist information including biographies, examples of art work, and other sources of information about the artist; registering to receive email alerts about objects offered at upcoming auctions, and emailing an antique object to a friend. Additional terms and conditions of use applicable to your use of the Services available at the Sites from time to time are or may be posted in relevant areas within the Sites or may be agreed upon by you and Invaluable in a separate written agreement (e.g. Subscription Agreement; Privacy Policy; auction house conditions of sale; all such additional terms and conditions, "Additional Agreements"). Together with this Agreement, such Additional Agreements will govern your use of those Services or any other services made available by us to you. We reserve the right, exercisable at our discretion, to change or discontinue without prior notice some or all of the Services we provide from time to time at the Sites, or change the applicable terms and conditions for your use of such Services as set forth in the Additional Agreements.
+                </span>
+
               </li>
               <li>
-                When accessing our website, the Customer must ensure you are at
-                least 15 years of age or access under a parent or legal
-                guardian's supervision. Customers guarantee that you have fully
-                the legal capacity of a natural person to establish and exercise
-                and perform civil transactions in accordance with the prevailing
-                regulations and laws.
+                <h5>1.4 Your Account.</h5>
+                <span>
+                Your Account is identified by your chosen user ID (your "User Name") and you are the only party authorized to use your User Name. You are responsible for maintaining the confidentiality of your registered account with us and any password(s) you have chosen or we may issue to you in connection with your access and use of the Services. You are responsible for all uses of your account, whether or not actually or expressly authorized by you. If you believe that your account and/or password(s) have been misused or compromised in any manner, please contact us immediately at support@Invaluable.com.
+                </span>
+ 
+              </li>
+              <li>
+                <h5>1.5 Covenant Regarding Your Account.</h5>
+                <span>
+                You agree that you will not maintain multiple registered accounts or User Names with us without our express written consent. If you wish to maintain a multiple account, you may submit a request to do so to support@Invaluable.com, including your primary User Name and your rationale for having multiple accounts.
+                </span>
+              </li>
+              <li>
+                <h5>1.6 Equipment..</h5>
+                <span>
+                You shall be solely responsible for obtaining and maintaining all telephone, communications, computer hardware and other equipment needed for access to and use of the Services and all charges of any description arising from or relating thereto.
+                </span>
+              </li>
+              <li>
+                <h5>1.7 Invaluable not involved in auctions/release.</h5>
+                <span>
+                We are not involved in actual transactions between users of the Sites or any "External Sites" as defined in section 5.1 below (whether such users are auction houses, appraisers, art or antiques dealers, trade members, curators, educators, antiques professionals, collectors or other third parties affiliated with Invaluable). We have no control over the quality or legality of the items displayed on our Sites or the truth or accuracy of the descriptions of such items, upcoming auction events or auction results displayed on our Sites. In the event that you have a dispute with any parties arising out of your use of the Sites you hereby release Invaluable from claims, demands and damages (actual and consequential) of every kind and nature, known and unknown, suspected and unsuspected, disclosed and undisclosed, arising out of or in any way connected with such disputes.
+                </span>
               </li>
             </ul>
           </div>
         </div>
         <div>
           <div>
-            <h3>ORDERS:</h3>
+            <h3>Section 2. Use of Content.</h3>
           </div>
           <div>
             <ul>
               <li>
-                Annam Gourmet offers 3 platforms to place online orders: Order
-                directly at the Annam Gourmet’s stores, via our hotline or
-                website ecommerce.
+                <h5>2.1 Copyright.</h5>
+                <span>
+                You acknowledge that the Services contain information, text, software, photographs, music, audio and video clips, graphics, links and other material (collectively, the "Content") that are protected by copyright, trademark or other proprietary rights of Invaluable or third parties. All Content used or made available through the Services is copyrighted as a collective work and/or compilation of Invaluable pursuant to applicable copyright law. Invaluable owns a copyright in the selection, coordination, arrangement and enhancement of such Content, as well as in the content original to Invaluable. You agree to comply with any additional copyright notices, information, or restrictions contained in any Content available on or accessed through the Services. Invaluable does not grant permission to users of its Services to reproduce images of original works of art.
+                </span>
               </li>
               <li>
-                Annam Gourmet has the right to refuse orders, notably in case of
-                unpaid invoices or insolvency.
+                <h5>2.2 Limited License.</h5>
+                <span>Subject to your compliance with the terms and conditions of this Agreement (including without limitation the restrictive terms and conditions set forth in this Section 4), and except to the extent otherwise expressly provided in an Additional Agreement between you and Invaluable, you are granted a limited non-exclusive, non-transferable, non-sub-licensable, and revocable license to access the Services and/or Content only for your own personal non-commercial use.
+You acknowledge that all Content included with the Services is provided for your own use. In no case shall you re-publish the Content in any form without express written permission of Invaluable. You will limit printouts for personal use of the material to a maximum of 35 individual text listings per individual. Reproduction of images is strictly prohibited without the written permission of both Invaluable and the originating auction house. Your right to use this product is limited to a single workstation unless an additional agreement for network access has been signed by both parties. You acknowledge that you are not permitted to sell or otherwise dispose the software and that no rights granted hereunder may be assigned or sublicensed by Licensee, any assignment or sublicense being void.
+You agree to maintain and/or reproduce all copyright and other notices contained in such Services and/or Content. You may not modify, publish, transmit, transfer or sell, resell, reproduce, create derivative works from, distribute, perform, display, or in any way exploit or commercially use any of the Services and/or Content, in whole or in part, except as expressly permitted in this Agreement or in an Additional Agreement between you and Invaluable. You acknowledge that you do not acquire any ownership rights by accessing or downloading copyrighted materials. You shall not store electronically any portion of any Services and/or Content. You may not collect and use e-mail addresses of other users of the Sites or other user account information, listings, or perform any form of data extraction or data-mining whatsoever. Except as expressly permitted by the copyright laws, no copying, storage, redistribution or publication of any of the Services and/or Content is permitted without the express permission of Invaluable or the owners of such Services and/or Content or their authorized persons, if other than Invaluable. You are responsible for complying with all applicable laws, rules, and regulations regarding your use of any such downloaded Content. Invaluable reserves all rights in the Content and Services not expressly granted hereunder.</span>
               </li>
               <li>
+                <h5>4.3 Invaluable is a trademark of Invaluable, LLC.</h5>
+                <span></span>
                 Exact full name, address, and phone number are strictly
                 requested.
               </li>
               <li>
+              <h5></h5>
+                <span></span>
                 Customers shall be advised not to place the same orders on the
                 website, via hotline, and at stores parallelly.
               </li>
               <li>
+              <h5></h5>
+                <span></span>
                 Orders made over the weekend might be a bit longer in delivery
                 according to the traffic
               </li>
               <li>
+              <h5></h5>
+                <span></span>
                 Orders made outside business hours will be processed the next
                 day
               </li>
               <li>
+              <h5></h5>
+                <span></span>
                 In case customers need the goods urgently, please contact the
                 store hotline to order directly, or visit the nearest store.
               </li>
