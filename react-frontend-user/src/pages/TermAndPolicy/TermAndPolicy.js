4c45565a1266949652302d91c84c18b97a8697fb
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-08-06 17:18:32 +0700

    add TermAndPolicy + css responsive

--- react-frontend-user/src/pages/TermAndPolicy/TermAndPolicy.js
+++ react-frontend-user/src/pages/TermAndPolicy/TermAndPolicy.js
@@ -3,59 +3,59 @@ import React, { Component } from "react";
 class TermAndCondition extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container term-wrapper">
         <div>
           <div>
-            <h1>GENERAL CONDITIONS:</h1>
-            <h5>INVALUABLE, INVALUABLE LIVE AUCTIONS AND INVALUABLE TIMED AUCTIONS TERMS OF USE AGREEMENT</h5>
+            <h1><b>GENERAL CONDITIONS:</b></h1>
+            <h5><b>INVALUABLE, INVALUABLE LIVE AUCTIONS AND INVALUABLE TIMED AUCTIONS TERMS OF USE AGREEMENT</b></h5>
             <span>This document describes terms and conditions applicable to your use of the services made available by Invaluable, LLC ("Invaluable", "we", or "us") at the websites Invaluable.com, VNHPOnlineAuction.com, and Infinite Bidding Sites (the "Sites").</span>
           </div>
 
-        <h3>Section 1. General.</h3>
+        <h3><b>Section 1. General.</b></h3>
           <div>
             <ul>
               <li>
-                <h5>1.1 Services.</h5>
+                <h5><b>1.1 Services.</b></h5>
                 <span>
                 This Terms and Conditions of Use Agreement (hereafter this "Agreement") sets forth the general terms and conditions that apply to the use by you of the Sites, including the features and services offered by us from time to time at or through the Sites (collectively with the Sites, the "Services"). BY USING THE SERVICES, YOU AGREE TO THE TERMS AND CONDITIONS CONTAINED HEREIN. This Agreement is a legally binding instrument between you and Invaluable, and describes your responsibilities in connection with your use of the Services and, among other things, limits the liability of Invaluable. Before using any of the Services, please read all of this Agreement carefully. By accessing or using any Services, you affirm that you are over 18 years of age and are otherwise capable of forming legally binding contracts, and that you agree to be legally bound and to abide by this Agreement. If you are under 18 years of age, or are otherwise incapable of forming legally binding contracts, or do not agree with any part of this Agreement, YOU MUST NOT ACCESS OR USE THE SERVICES.
                 </span>
 
               </li>
               <li>
-                <h5>1.2 Amendments.</h5>
+                <h5><b>1.2 Amendments.</b></h5>
                 <span>
                 We reserve the right, exercisable in our sole discretion, to change, modify, add to, subtract from, or otherwise amend the terms and conditions of this Agreement at any time. Except as otherwise stated below, all changes, modifications, or other amendments shall be effective on a prospective basis once they are posted on our Sites. This Agreement may not otherwise be amended except in a writing signed by you and Invaluable. Continued use of the Services by you constitutes your binding acceptance of this Agreement, including any changes or modifications made by us as described above. You agree to review the terms and conditions of this Agreement periodically to become aware of such revisions and to review your compliance with them. If at any time the terms and conditions of this Agreement are no longer acceptable to you, you must immediately cease all use of the Services. The right to access and use the Services is personal to you and is not transferable to any other person or entity.
                 </span>
  
               </li>
               <li>
-                <h5>1.3 Additional Agreements.</h5>
+                <h5><b>1.3 Additional Agreements.</b></h5>
                 <span>
                 The Services include, among other things, such services as: contacting or placing bids with auction houses by email or through a live bidding console; searching and accessing past and upcoming auction records including, when available, object photos and estimated and realized prices; looking up or searching for artist information including biographies, examples of art work, and other sources of information about the artist; registering to receive email alerts about objects offered at upcoming auctions, and emailing an antique object to a friend. Additional terms and conditions of use applicable to your use of the Services available at the Sites from time to time are or may be posted in relevant areas within the Sites or may be agreed upon by you and Invaluable in a separate written agreement (e.g. Subscription Agreement; Privacy Policy; auction house conditions of sale; all such additional terms and conditions, "Additional Agreements"). Together with this Agreement, such Additional Agreements will govern your use of those Services or any other services made available by us to you. We reserve the right, exercisable at our discretion, to change or discontinue without prior notice some or all of the Services we provide from time to time at the Sites, or change the applicable terms and conditions for your use of such Services as set forth in the Additional Agreements.
                 </span>
 
               </li>
               <li>
-                <h5>1.4 Your Account.</h5>
+                <h5><b>1.4 Your Account.</b></h5>
                 <span>
                 Your Account is identified by your chosen user ID (your "User Name") and you are the only party authorized to use your User Name. You are responsible for maintaining the confidentiality of your registered account with us and any password(s) you have chosen or we may issue to you in connection with your access and use of the Services. You are responsible for all uses of your account, whether or not actually or expressly authorized by you. If you believe that your account and/or password(s) have been misused or compromised in any manner, please contact us immediately at support@Invaluable.com.
                 </span>
  
               </li>
               <li>
-                <h5>1.5 Covenant Regarding Your Account.</h5>
+                <h5><b>1.5 Covenant Regarding Your Account.</b></h5>
                 <span>
                 You agree that you will not maintain multiple registered accounts or User Names with us without our express written consent. If you wish to maintain a multiple account, you may submit a request to do so to support@Invaluable.com, including your primary User Name and your rationale for having multiple accounts.
                 </span>
               </li>
               <li>
-                <h5>1.6 Equipment..</h5>
+                <h5><b>1.6 Equipment..</b></h5>
                 <span>
                 You shall be solely responsible for obtaining and maintaining all telephone, communications, computer hardware and other equipment needed for access to and use of the Services and all charges of any description arising from or relating thereto.
                 </span>
               </li>
               <li>
-                <h5>1.7 Invaluable not involved in auctions/release.</h5>
+                <h5><b>1.7 Invaluable not involved in auctions/release.</b></h5>
                 <span>
                 We are not involved in actual transactions between users of the Sites or any "External Sites" as defined in section 5.1 below (whether such users are auction houses, appraisers, art or antiques dealers, trade members, curators, educators, antiques professionals, collectors or other third parties affiliated with Invaluable). We have no control over the quality or legality of the items displayed on our Sites or the truth or accuracy of the descriptions of such items, upcoming auction events or auction results displayed on our Sites. In the event that you have a dispute with any parties arising out of your use of the Sites you hereby release Invaluable from claims, demands and damages (actual and consequential) of every kind and nature, known and unknown, suspected and unsuspected, disclosed and undisclosed, arising out of or in any way connected with such disputes.
                 </span>
@@ -65,156 +65,34 @@ class TermAndCondition extends Component {
         </div>
         <div>
           <div>
-            <h3>Section 2. Use of Content.</h3>
+            <h3><b>Section 2. Use of Content.</b></h3>
           </div>
           <div>
             <ul>
               <li>
-                <h5>2.1 Copyright.</h5>
+                <h5><b>2.1 Copyright.</b></h5>
                 <span>
                 You acknowledge that the Services contain information, text, software, photographs, music, audio and video clips, graphics, links and other material (collectively, the "Content") that are protected by copyright, trademark or other proprietary rights of Invaluable or third parties. All Content used or made available through the Services is copyrighted as a collective work and/or compilation of Invaluable pursuant to applicable copyright law. Invaluable owns a copyright in the selection, coordination, arrangement and enhancement of such Content, as well as in the content original to Invaluable. You agree to comply with any additional copyright notices, information, or restrictions contained in any Content available on or accessed through the Services. Invaluable does not grant permission to users of its Services to reproduce images of original works of art.
                 </span>
               </li>
               <li>
-                <h5>2.2 Limited License.</h5>
+                <h5><b>2.2 Limited License.</b></h5>
                 <span>Subject to your compliance with the terms and conditions of this Agreement (including without limitation the restrictive terms and conditions set forth in this Section 4), and except to the extent otherwise expressly provided in an Additional Agreement between you and Invaluable, you are granted a limited non-exclusive, non-transferable, non-sub-licensable, and revocable license to access the Services and/or Content only for your own personal non-commercial use.
 You acknowledge that all Content included with the Services is provided for your own use. In no case shall you re-publish the Content in any form without express written permission of Invaluable. You will limit printouts for personal use of the material to a maximum of 35 individual text listings per individual. Reproduction of images is strictly prohibited without the written permission of both Invaluable and the originating auction house. Your right to use this product is limited to a single workstation unless an additional agreement for network access has been signed by both parties. You acknowledge that you are not permitted to sell or otherwise dispose the software and that no rights granted hereunder may be assigned or sublicensed by Licensee, any assignment or sublicense being void.
 You agree to maintain and/or reproduce all copyright and other notices contained in such Services and/or Content. You may not modify, publish, transmit, transfer or sell, resell, reproduce, create derivative works from, distribute, perform, display, or in any way exploit or commercially use any of the Services and/or Content, in whole or in part, except as expressly permitted in this Agreement or in an Additional Agreement between you and Invaluable. You acknowledge that you do not acquire any ownership rights by accessing or downloading copyrighted materials. You shall not store electronically any portion of any Services and/or Content. You may not collect and use e-mail addresses of other users of the Sites or other user account information, listings, or perform any form of data extraction or data-mining whatsoever. Except as expressly permitted by the copyright laws, no copying, storage, redistribution or publication of any of the Services and/or Content is permitted without the express permission of Invaluable or the owners of such Services and/or Content or their authorized persons, if other than Invaluable. You are responsible for complying with all applicable laws, rules, and regulations regarding your use of any such downloaded Content. Invaluable reserves all rights in the Content and Services not expressly granted hereunder.</span>
               </li>
               <li>
-                <h5>4.3 Invaluable is a trademark of Invaluable, LLC.</h5>
-                <span></span>
-                Exact full name, address, and phone number are strictly
-                requested.
+                <h5><b>2.3 Invaluable is a trademark of Invaluable, LLC.</b></h5>
+                <span>
+                All rights reserved. All other trademarks associated with product and company names that may appear in connection with the posting of items for sale are the property of their respective owners. Use of such third party trademarks does not suggest or imply any affiliation with or endorsement by those third parties.
+                </span>
               </li>
               <li>
-              <h5></h5>
-                <span></span>
-                Customers shall be advised not to place the same orders on the
-                website, via hotline, and at stores parallelly.
-              </li>
-              <li>
-              <h5></h5>
-                <span></span>
-                Orders made over the weekend might be a bit longer in delivery
-                according to the traffic
-              </li>
-              <li>
-              <h5></h5>
-                <span></span>
-                Orders made outside business hours will be processed the next
-                day
-              </li>
-              <li>
-              <h5></h5>
-                <span></span>
-                In case customers need the goods urgently, please contact the
-                store hotline to order directly, or visit the nearest store.
-              </li>
-            </ul>
-          </div>
-
-          <div>
-            <div>
-              <h3>PAYMENT POLICY:</h3>
-            </div>
-            <div>
-              <p>
-                - Step 1: Select the product you want to buy - Click cart icon
-                or “Add to cart”
-              </p>
-              <br />
-              <p>
-                - Step 2: After choosing items, please check price and quantity
-                of items carefully before clicking “Process to check out”
-              </p>
-              <br />
-              <p>- Step 3: Fulfill necessary information of order form</p>
-              <br />
-              <p>
-                - Step 4: Choose the “Shipping method”. Then we offer you the
-                following payment methods:
-              </p>
-              <br />
-              <p>
-                a. Credit card on delivery: pay directly to the shipper when you
-                receive the product.
-              </p>
-              <br />
-              <p>b. Transfer by bank/ATM before your order is shipped.</p>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <div>
-            <h3>SHIPPING POLYCY:</h3>
-          </div>
-          <div>
-            <ul>
-              <li>
-                Annam Gourmet makes every possible effort to ensure delivery to
-                customers as soon as possible by delivery service.
-              </li>
-              <li>
-                Free shipping for orders from 1.000.000 VND, applied for the
-                areas below 10km from Estore within Hanoi, HCMC.
-              </li>
-              <li>
-                Other delivery fees will be different, depending on the area.
-                The specific fee will be shown when you proceed your order on
-                the website.
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div>
-          <div>
-            <h3>CANCELLATION POLICY:</h3>
-          </div>
-          <div>
-            <ul>
-              <li>
-                Please contact us via Hotline: 1900 63 64 31 to be supported for
-                cancellation order
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          <div>
-            <h3>DELIVERY POLICY:</h3>
-          </div>
-          <div>
-            <ul>
-              <li>
-                Annam Gourmet makes every possible effort to ensure the fastest
-                delivery to our customers by Annam Gourmet ’s drivers and
-                delivery service.
-              </li>
-              <li>
-                Free shipping for orders from 1.000.000 VND and below 10 km
-              </li>
-              <li>
-                For others under 1.000.000 VND, the shipping fee will be shown
-                when you process your order on the website.
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          <div>
-            <h3>DELIVERY TIME:</h3>
-          </div>
-          <div>
-            <ul>
-              <li>
-                Central areas of HCMC, Hanoi with delivery express: 1-3 hours
-              </li>
-              <li>
-                Out of delivery zones and other provinces: : Depends on the
-                distances
+                <h5><b>2.4 Uploading or Posting Content.</b></h5>
+                <span>
+                You will not upload, post, or otherwise make available through the Services any Content protected by copyright, trademark, or other proprietary right without the express permission of the owner of the copyright, trademark, or other proprietary right and the burden of determining that any material is not protected by copyright rests with you. You shall be solely liable for any damage resulting from any infringement of copyrights, proprietary rights, or any other harm resulting from such a submission. By submitting Content to any public area of the Sites, you hereby grant, or warrant that the owner of such Content has expressly granted, Invaluable a royalty-free, perpetual, irrevocable, non-exclusive right and license to use, reproduce, modify, adapt, publish, translate, publicly display and distribute such Content (in whole or in part) worldwide and/or to incorporate it in other works in any form, media or technology now known or hereafter developed for the full term of any copyright that may exist in such Content. You also permit any other user of the Services to access, view, store, or reproduce such Content for that user's personal use.
+4.5 Beneficiaries. The foregoing provisions of this Section 3 are for the benefit of Invaluable, its subsidiaries, affiliates and its third party content providers and licensors and each shall have the right to assert and enforce such provisions directly or on its own behalf.
+                </span>
               </li>
             </ul>
           </div>
